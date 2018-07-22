6a4c9f8c1d42e46fe07fa7dd3abc8968bad0c091
Amee Covarrubias <ameecova@yahoo.com>
2018-07-22 07:08:41 +0000

    automatically backed up by learn

--- lib/cruise_ship.rb
+++ lib/cruise_ship.rb
@@ -8,6 +8,7 @@
 
 def select_winner(passengers)
   # add the code snippet here!
+  winner = ""
   passengers.each do |room, name| 
     if room == :suite_a && name[0] == "A"? 
       winner = name  
